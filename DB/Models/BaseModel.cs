134f4aa7cee1d9dbab2fb35d6ff9ab3e65f76852
Nolan Blew <nolanblew@hotmail.com>
2018-10-11 21:00:57 -0700

    Add database

---
+++ DB/Models/BaseModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Food_Journal.DB.Models
+{
+    public abstract class BaseModel
+    {
+        public int Id { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+    }
+}
