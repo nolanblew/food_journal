5dd6aa10fd619429c6f2b061217d3d2f55454a87
Nolan Blew <nolanblew@hotmail.com>
2018-10-15 22:36:04 -0700

    Add client api with users

---
+++ Food Journal.ClientApi/Controllers/BaseController.cs
@@ -0,0 +1,89 @@
+﻿using Food_Journal.DB.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Food_Journal.ClientApi.Controllers
+{
+    public abstract class BaseController<T> where T : BaseModel
+    {
+        const string BASE_ENDPOINT = "https://api-foodjournal.azurewebsites.net/api";
+
+        /// <summary>
+        /// The endpoint of the controller (without the slashes). ex: users
+        /// </summary>
+        protected virtual string ControllerEndpoint { get; }
+
+        protected Task<List<T>> _ListAsync()
+        {
+            return _RequestApi<List<T>>("/");
+        }
+
+        protected Task<T> _ShowAsync(int id)
+        {
+            return _RequestApi<T>($"/{id}");
+        }
+
+        public async Task<T> _CreateAsync(T item)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new
+                    Uri("https://api-foodjournal.azurewebsites.net");
+
+                var path = $"{BASE_ENDPOINT}/{ControllerEndpoint}/";
+                var apiResult = await client.PostAsJsonAsync<T>(path, item);
+                var json = await apiResult.Content.ReadAsStringAsync();
+
+                var result = JsonConvert.DeserializeObject<T>(json);
+                return result;
+            }
+        }
+
+        public async Task<T> _UpdateAsync(int id, T item)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new
+                    Uri("https://api-foodjournal.azurewebsites.net");
+
+                var path = $"{BASE_ENDPOINT}/{ControllerEndpoint}/{id}";
+                var apiResult = await client.PutAsJsonAsync<T>(path, item);
+                var json = await apiResult.Content.ReadAsStringAsync();
+
+                var result = JsonConvert.DeserializeObject<T>(json);
+                return result;
+            }
+        }
+
+        public async Task<bool> _DeleteAsync(int id)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new
+                    Uri("https://api-foodjournal.azurewebsites.net");
+
+                var path = $"{BASE_ENDPOINT}/{ControllerEndpoint}/{id}";
+                var apiResult = await client.DeleteAsync(path);
+                
+                return apiResult.IsSuccessStatusCode;
+            }
+        }
+
+        protected async Task<TOut> _RequestApi<TOut>(string relativeEndpoint)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new
+                    Uri("https://api-foodjournal.azurewebsites.net");
+
+                var path = $"{BASE_ENDPOINT}/{ControllerEndpoint}/{relativeEndpoint}";
+                var json = await client.GetStringAsync(path);
+                var result = JsonConvert.DeserializeObject<TOut>(json);
+                return result;
+            }
+        }
+    }
+}
---
+++ Food Journal.ClientApi/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using Food_Journal.DB.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Food_Journal.ClientApi.Controllers
+{
+    public interface IUserController
+    {
+        Task<User> CreateUser(User user);
+        Task<List<User>> GetUsers();
+        Task<User> ShowUser(int id);
+        Task<User> UpdateUser(int id, User user);
+    }
+
+    public class UserController : BaseController<User>, IUserController
+    {
+        protected override string ControllerEndpoint => "users";
+
+        public Task<List<User>> GetUsers()
+        {
+            return _ListAsync();
+        }
+
+        public Task<User> ShowUser(int id)
+        {
+            return _ShowAsync(id);
+        }
+
+        public Task<User> CreateUser(User user)
+        {
+            return _CreateAsync(user);
+        }
+
+        public Task<User> UpdateUser(int id, User user)
+        {
+            return _UpdateAsync(id, user);
+        }
+    }
+}
